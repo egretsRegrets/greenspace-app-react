25e96ab88bc04891352e05c2c53574eb63c31867
mercantilism <samuel.galen.gardner@gmail.com>
2017-12-08 11:05:16 -0800

    routing and test component - Greenspace detail

--- js/App.jsx
+++ js/App.jsx
@@ -2,11 +2,13 @@
 
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import type { Match } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import Landing from './Landing';
 import GlobalHeader from './GlobalHeader';
 import Greenspaces from './Greenspaces';
+import GreenspaceDetail from './GreenspaceDetail';
 import preload from '../data.json';
 
 const FourOhFour = () => <h1>404 - Uh Oh Nothing at this address - 404</h1>;
@@ -21,6 +23,15 @@ const App = () => (
           <Route
             path="/greenspaces"
             component={props => <Greenspaces greenspaces={preload.greenspaces} {...props} />}
+          />
+          <Route
+            path="/greenspace/:id"
+            component={(props: { match: Match }) => {
+              const selectedGreenspace = preload.greenspaces.find(
+                (greenspace: Greenspace) => greenspace.id === props.match.params.id
+              );
+              return <GreenspaceDetail greenspace={selectedGreenspace} {...props} />;
+            }}
           />
           <Route component={FourOhFour} />
         </Switch>
---
+++ js/GreenspaceDetail.jsx
@@ -0,0 +1,7 @@
+// @flow
+
+import React from 'react';
+
+const GreenspaceDetail = (props: { greenspace: Greenspace }) => <h1>{props.greenspace.name}</h1>;
+
+export default GreenspaceDetail;
