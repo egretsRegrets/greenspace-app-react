bb0c67fa75d2d489cf3ca34b6a6d9a1e00f3fe9e
mercantilism <samuel.galen.gardner@gmail.com>
2017-12-12 18:41:43 -0800

    user name in global header linked to user profile based on user.id

--- js/GlobalHeader.jsx
+++ js/GlobalHeader.jsx
@@ -2,9 +2,10 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { connect, MapStateToProps } from 'react-redux';
+import preload from '../data.json';
 
-const GlobalHeader = (props: { userName: string }) => (
+const GlobalHeader = (props: { selectedUser: User }) => (
   <header className="fixed w-100 flex items-center h3 pa4 ba bw4 bg-green green">
     <nav className="w-50 flex items-center h3">
       <Link className="mr3 f2 white avenir no-underline b" to="/">
@@ -18,12 +19,18 @@ const GlobalHeader = (props: { userName: string }) => (
       </Link>
     </nav>
     <nav className="w-50 flex flex-row-reverse items-center h3">
-      <Link className="pt2 f4 white avenir no-underline b dim" to="/">
-        {props.userName ? props.userName : 'Login'}
+      <Link
+        className="pt2 f4 white avenir ttc no-underline b dim"
+        to={props.selectedUser ? `/user/${props.selectedUser.id}` : '/'}
+      >
+        {props.selectedUser ? props.selectedUser.userName : 'Login'}
       </Link>
     </nav>
   </header>
 );
-const mapStateToProps = state => ({ userName: state.user.name });
+const mapStateToProps: MapStateToProps<*, *, *> = state => {
+  const selectedUser = preload.users.find((user: User) => user.id === state.user.id);
+  return { selectedUser };
+};
 
 export default connect(mapStateToProps)(GlobalHeader);
