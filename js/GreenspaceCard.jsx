2991f19c368384ce6b6aa8aaf2f44f658f2dbf4b
mercantilism <samuel.galen.gardner@gmail.com>
2017-12-14 23:36:39 -0800

    various style changes, normalized height

--- js/GreenspaceCard.jsx
+++ js/GreenspaceCard.jsx
@@ -34,7 +34,7 @@ const GreenspaceCard = (props: Greenspace) => {
   }
   return (
     <div className="w-25 mb3 pa3">
-      <section className="bg-near-white br3">
+      <section className="h-100 bg-near-white br3 shadow-5">
         <div className="h4 overflow-hidden">{spaceMainImage}</div>
         <Link
           to={`/greenspace/${props.id}`}
@@ -42,18 +42,20 @@ const GreenspaceCard = (props: Greenspace) => {
         >
           See Greenspace
         </Link>
-        <div className="ph3 pt0 pb2 tc">
-          <h3 className="mv0 f4 lh-copy ttc avenir">{props.name}</h3>
-          <p className="mt2 mb3 f5 lh-copy helvetica">{props.address}</p>
-          <div className="flex ph1 avenir">
-            <ul className="w-50 mt0 pl3 tl">
-              {props.tags.map(tag => (
-                <li key={tag} className="mv1 ttc f6 dark-green">
-                  {tag}
-                </li>
-              ))}
-            </ul>
-            <div className="w-50 f7 tc">{seekingFarmer}</div>
+        <div className="pt0 pb2 tc">
+          <h3 className="mv0 ph3  f4 lh-copy ttc avenir">{props.name}</h3>
+          <h5 className="mt2 mb3 ph3 f5 lh-title normal avenir">{props.address}</h5>
+          <div className="bt b--black-10">
+            <div className="flex mt4 mb2 ph4 avenir">
+              <ul className="w-50 mt0 pl3 tl">
+                {props.tags.map(tag => (
+                  <li key={tag} className="mv1 ttc f6 dark-green">
+                    {tag}
+                  </li>
+                ))}
+              </ul>
+              <div className="w-50 f7 tc">{seekingFarmer}</div>
+            </div>
           </div>
         </div>
       </section>
