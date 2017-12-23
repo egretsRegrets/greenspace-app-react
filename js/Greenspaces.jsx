b90f35322a695241d91a91ac223cb7806f01f034
mercantilism <samuel.galen.gardner@gmail.com>
2017-12-22 22:17:02 -0600

    pagination incorporated in Greenspaces.jsx

--- js/Greenspaces.jsx
+++ js/Greenspaces.jsx
@@ -1,14 +1,46 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 import GreenspaceCard from './GreenspaceCard';
+import { NextPreviousBtns } from './utilComponents/PageControls';
+import { PaginationSlice } from './utils';
 
-const Greenspaces = (props: { greenspaces: Array<Greenspace> }) => (
-  <div className="pv6 ph5">
-    <section className="flex flex-wrap justify-between pv4">
-      {props.greenspaces.map((greenspace: Greenspace) => <GreenspaceCard {...greenspace} key={greenspace.id} />)}
-    </section>
-  </div>
-);
+// $FlowFixMe
+class Greenspaces extends Component {
+  constructor() {
+    super();
+    // $FlowFixMe
+    this.state = {
+      pageNumber: 1
+    };
+  }
+
+  props: { greenspaces: Array<Greenspace> };
+
+  updatePage = (val: number) =>
+    // $FlowFixMe
+    this.setState({ pageNumber: parseInt(val, 10) });
+
+  render() {
+    const cardsPerPage = 4;
+    return (
+      <div className="pv6 ph5">
+        <section className="flex flex-wrap justify-between pv4">
+          {// $FlowFixMe
+          PaginationSlice(this.props.greenspaces, this.state.pageNumber, cardsPerPage).map((greenspace: Greenspace) => (
+            <GreenspaceCard {...greenspace} key={greenspace.id} />
+          ))}
+        </section>
+        <NextPreviousBtns
+          // $FlowFixMe
+          pageNumber={this.state.pageNumber}
+          dataLength={this.props.greenspaces.length}
+          cardsPerPage={cardsPerPage}
+          clickHandler={this.updatePage}
+        />
+      </div>
+    );
+  }
+}
 
 export default Greenspaces;
