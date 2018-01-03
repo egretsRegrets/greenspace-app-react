eb0b952797c5ccb2abc91a0daaba2f69a503b209
mercantilism <samuel.galen.gardner@gmail.com>
2018-01-03 13:47:09 -0800

    more farmer info displayed in that user view

--- js/UserFarmingView.jsx
+++ js/UserFarmingView.jsx
@@ -9,10 +9,29 @@ const UserFarmingView = (props: { farmer: FarmerBrief }) => {
     (greenspace: Greenspace) =>
       typeof props.farmer.properties !== 'undefined' && props.farmer.properties.includes(greenspace.id)
   );
+  const farmingGreenspacesTitle = (
+    <h3 className="mt5 mb0 f3 avenir near-black">{`Greenspaces ${props.farmer.userName} is Farming`}</h3>
+  );
+  const farmingGreenspacesList = <Greenspaces greenspaces={farmingGreenspaces} />;
+  let farmingSkillList;
+  if (props.farmer.skills) {
+    farmingSkillList = (
+      <div>
+        <h5>Farming Skills:</h5>
+        {props.farmer.skills.map((skill: string) => <li key={skill}>{skill}</li>)}
+      </div>
+    );
+  }
   return (
     <section>
-      <h3 className="f3 avenir near-black pl5">Farming</h3>
-      <Greenspaces greenspaces={farmingGreenspaces} />
+      <div className="pl5">
+        <h4 className="avenir f4 ttc">{`${props.farmer.experience} farmer`}</h4>
+        {farmingSkillList}
+        <p className="lh-copy avenir">{props.farmer.bio}</p>
+        {farmingGreenspaces.length ? farmingGreenspacesTitle : null}
+      </div>
+      {farmingGreenspaces.length ? farmingGreenspacesList : null}
+      <code>{JSON.stringify(props.farmer)}</code>
     </section>
   );
 };
