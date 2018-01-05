3c656e32151b2fb368e34751a9f7c88d074080a0
mercantilism <samuel.galen.gardner@gmail.com>
2018-01-04 17:04:53 -0800

    style and data display

--- js/UserGreenspacesView.jsx
+++ js/UserGreenspacesView.jsx
@@ -10,10 +10,39 @@ const UserGreenspaceView = (props: { greenspaceOwner: GreenspaceOwnerBrief }) =>
       typeof props.greenspaceOwner.ownedPropertyIDs !== 'undefined' &&
       props.greenspaceOwner.ownedPropertyIDs.includes(greenspace.id)
   );
+  const lendingGreenspacesTitle = (
+    <h3 className="mt5 mb0 f3 avenir near-black">{`Greenspaces ${props.greenspaceOwner.userName} is Lending`}</h3>
+  );
+  const lendingGreenspacesList = <Greenspaces greenspaces={ownedGreenspaces} />;
+  let landownerParticipationInfo;
+  let participationColor: string;
+  if (props.greenspaceOwner.desiredLandOwnerParticipation) {
+    if (props.greenspaceOwner.desiredLandOwnerParticipation === 'hands-off') {
+      participationColor = 'near-white';
+    } else if (props.greenspaceOwner.desiredLandOwnerParticipation === 'helping hand') {
+      participationColor = 'near-black';
+    } else {
+      participationColor = 'green';
+    }
+  }
+  if (props.greenspaceOwner.desiredLandOwnerParticipation) {
+    landownerParticipationInfo = (
+      <div>
+        <h5 className="mb3 f5 avenir i">Farming Participation:</h5>
+        <h5 className={`mt0 dib pv2 ph3 ba b--${participationColor} bw2 f5 avenir`}>
+          {props.greenspaceOwner.desiredLandOwnerParticipation}
+        </h5>
+      </div>
+    );
+  }
   return (
     <section>
-      <h3 className="f3 avenir near-black pl5">Greenspace</h3>
-      <Greenspaces greenspaces={ownedGreenspaces} />
+      <div className="ph5">
+        {landownerParticipationInfo}
+        <p className="mw8 lh-copy avenir">{props.greenspaceOwner.bio}</p>
+        {ownedGreenspaces.length ? lendingGreenspacesTitle : null}
+      </div>
+      {ownedGreenspaces.length ? lendingGreenspacesList : null}
     </section>
   );
 };
