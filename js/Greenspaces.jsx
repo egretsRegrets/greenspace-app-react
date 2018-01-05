347b6de14c154ef15522bbab40285b34f7651996
mercantilism <samuel.galen.gardner@gmail.com>
2018-01-05 15:11:24 -0800

    to statefull - NextPreviousBtns added, passing pagination depended arguments to GreenspaceCardList component

--- js/Greenspaces.jsx
+++ js/Greenspaces.jsx
@@ -1,12 +1,49 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 import GreenspaceCardList from './GreenspaceCardList';
+import { NextPreviousBtns } from './utilComponents/PageControls';
 
-const GreenSpaces = (props: { greenSpaces: Array<Greenspace> }) => (
-  <section>
-    <GreenspaceCardList greenspaceCardList={props.greenSpaces} />
-  </section>
-);
+// $FlowFixMe
+class Greenspaces extends Component {
+  constructor() {
+    super();
+    // $FlowFixMe
+    this.state = {
+      pageNumber: 1
+    };
+  }
 
-export default GreenSpaces;
+  props: {
+    greenspaces: Array<Greenspace>
+  };
+
+  updatePage = (val: number) =>
+    // $FlowFixMe
+    this.setState({ pageNumber: parseInt(val, 10) });
+
+  render() {
+    const cardsPerPage = 8;
+    return (
+      <section>
+        <GreenspaceCardList
+          greenspaceCardList={this.props.greenspaces}
+          // $FlowFixMe
+          currentPageNumber={this.state.pageNumber}
+          cardsPerPage={cardsPerPage}
+        />
+        <div className="pl5">
+          <NextPreviousBtns
+            // $FlowFixMe
+            pageNumber={this.state.pageNumber}
+            dataLength={this.props.greenspaces.length}
+            cardsPerPage={cardsPerPage}
+            clickHandler={this.updatePage}
+          />
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Greenspaces;
