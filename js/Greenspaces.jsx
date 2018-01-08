2ef8bc3e7f24285d4e5c399ea026eb05dfef532d
mercantilism <samuel.galen.gardner@gmail.com>
2018-01-08 00:11:41 -0800

    filter buttons dispatching working greenspacesFilters payload to redux store

--- js/Greenspaces.jsx
+++ js/Greenspaces.jsx
@@ -1,7 +1,10 @@
 // @flow
 
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import setGreenspacesFilters from './actionCreators';
 import GreenspaceCardList from './GreenspaceCardList';
+import FilterButton from './utilComponents/FilterButton';
 import { NextPreviousBtns } from './utilComponents/PageControls';
 
 // $FlowFixMe
@@ -15,15 +18,20 @@ class Greenspaces extends Component {
   }
 
   props: {
-    greenspaces: Array<Greenspace>
+    greenspaces: Array<Greenspace>,
+    filters: greenspacesFilters,
+    setFilters: Function
   };
 
   updatePage = (val: number) =>
     // $FlowFixMe
     this.setState({ pageNumber: parseInt(val, 10) });
 
+  updateFilters = event => this.props.setFilters(event.target.value, this.props.filters);
+
   render() {
     const cardsPerPage = 8;
+    const filterBtnsPlotSizeTxt = ['Any', 'Large Plot', 'Micro Plot', 'Backyard', 'frontyard', 'Full Yard'];
     return (
       <section>
         <div className="pt4 pb0">
@@ -43,19 +51,20 @@ class Greenspaces extends Component {
                 <path d="M1 18 L5 14 L16 24 L27 14 L31 18 L16 32 Z" />
               </svg>
             </button>
-
             <div className="flex items-center">
               <div className="mr4 flex justify-start">
                 <button className="mr3 pl1 pr3 pv1 bg-transparent bt-0 br-0 bb bl-0 bw2 b--green fw6 f5 avenir tl pointer">
                   Plot Size
                 </button>
                 <div className="flex justify-start">
-                  <button className="bw0 bg-transparent avenir f6 pointer">All</button>
-                  <button className="bw0 bg-transparent avenir f6 pointer">Large Plot</button>
-                  <button className="bw0 bg-transparent avenir f6 pointer">Micro Plot</button>
-                  <button className="bw0 bg-transparent avenir f6 pointer">Backyard</button>
-                  <button className="bw0 bg-transparent avenir f6 pointer">Front-Yard</button>
-                  <button className="bw0 bg-transparent avenir f6 pointer">Full-Yard</button>
+                  {Object.keys(this.props.filters.plotSize).map((filterKey: string, index) => (
+                    <FilterButton
+                      key={filterKey}
+                      val={filterKey}
+                      text={filterBtnsPlotSizeTxt[index]}
+                      updateFilter={this.updateFilters}
+                    />
+                  ))}
                 </div>
               </div>
               <div className="mr4 flex justify-start">
@@ -63,9 +72,14 @@ class Greenspaces extends Component {
                   Seeking Farmers
                 </button>
                 <div className="flex justify-start">
-                  <button className="bw0 bg-transparent avenir f6 pointer">Either</button>
-                  <button className="bw0 bg-transparent avenir f6 pointer">Yes</button>
-                  <button className="bw0 bg-transparent avenir f6 pointer">No</button>
+                  {Object.keys(this.props.filters.seekingFarmer).map((filterKey: string) => (
+                    <FilterButton
+                      key={filterKey}
+                      val={filterKey}
+                      text={filterKey.charAt(0).toUpperCase() + filterKey.slice(1)}
+                      updateFilter={this.updateFilters}
+                    />
+                  ))}
                 </div>
               </div>
             </div>
@@ -91,4 +105,60 @@ class Greenspaces extends Component {
   }
 }
 
-export default Greenspaces;
+function newFilters(filter: string, filtersType: 'plotSize' | 'seekingFarmer', curFilters: greenspacesFilters) {
+  if (filter === 'any' || filter === 'either') {
+    if (Object.values(curFilters[filtersType]).includes(false)) {
+      const targetFilters = curFilters[filtersType];
+      Object.keys(targetFilters).forEach(key => {
+        // $FlowFixMe
+        if (targetFilters[key] === false) {
+          // $FlowFixMe
+          targetFilters[key] = true;
+        }
+      });
+      const updatedFilters = curFilters;
+      // $FlowFixMe
+      updatedFilters[filtersType] = targetFilters;
+      return updatedFilters;
+    }
+    // if all of the filters of the filter type are already true, we just return curFilters
+    return curFilters;
+  }
+  const targetFilters = curFilters[filtersType];
+  // flip the bool of the filter in target
+  targetFilters[filter] = !curFilters[filtersType][filter];
+  // 'any' or 'either' will be index 0 of targetFilters,
+  // if index 0 is true and any values of targetFilters props are false, index 0 should be flipped.
+  if (Object.values(targetFilters)[0] === true && Object.values(targetFilters).includes(false)) {
+    // $FlowFixMe
+    targetFilters[Object.keys(targetFilters)[0]] = false;
+  }
+  // if index 0 is false, and the values of targetFilters sliced except for them - index 0 - don't include false, index 0 should be flipped
+  if (
+    Object.values(targetFilters)[0] === false &&
+    !Object.values(targetFilters)
+      .slice(1)
+      .includes(false)
+  ) {
+    // $FlowFixMe
+    targetFilters[Object.keys(targetFilters)[0]] = true;
+  }
+  const updatedFilters = curFilters;
+  // $FlowFixMe
+  updatedFilters[filtersType] = targetFilters;
+  return updatedFilters;
+}
+
+const mapStateToProps = state => ({ filters: state.greenspacesFilters });
+const mapDispatchToProps = (dispatch: Function) => ({
+  setFilters(filter, filters) {
+    // selecting the correct filter type, plotSize vs seekingFarmer based on filter button val
+    let filterType = 'seekingFarmer';
+    if (!Object.keys(filters[filterType]).includes(filter)) {
+      filterType = 'plotSize';
+    }
+    dispatch(setGreenspacesFilters(newFilters(filter, filterType, filters)));
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Greenspaces);
