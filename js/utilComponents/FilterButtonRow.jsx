caf1b107f231a847f26dbebbf31db3db30e2abc9
mercantilism <samuel.galen.gardner@gmail.com>
2018-01-13 17:23:10 -0800

    using filterState to pass val throug filterSelected in FilterButton props

--- js/utilComponents/FilterButtonRow.jsx
+++ js/utilComponents/FilterButtonRow.jsx
@@ -5,6 +5,7 @@ import FilterButton from './FilterButton';
 
 const FilterButtonRow = (props: {
   filter: string,
+  filterState: {},
   filterOptions: Array<string>,
   changeFilter: Function,
   btnTextArr?: Array<string>,
@@ -13,8 +14,10 @@ const FilterButtonRow = (props: {
 }) => {
   // pass filterValue and target filter up to parent
   const passFilterValue = event => props.changeFilter(event.target.value, props.filter);
+  const filterStateVals = Object.keys(props.filterState).map((filterKey: string) => props.filterState[filterKey]);
   const binaryBothBtn = (
     <FilterButton
+      filterSelected={!filterStateVals.includes(false)}
       key="binaryBoth"
       val="binaryBoth"
       text={props.binaryBothBtnText ? props.binaryBothBtnText : 'Either'}
@@ -26,6 +29,7 @@ const FilterButtonRow = (props: {
       {props.includesBinaryBoth ? binaryBothBtn : null}
       {props.filterOptions.map((filter: string, index) => (
         <FilterButton
+          filterSelected={props.filterState[filter]}
           key={filter}
           val={filter}
           // $FlowFixMe - possibly undefined value sufficiently covered by defaultProps
