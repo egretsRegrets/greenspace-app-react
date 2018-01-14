ba5e2cc07dc5b3f32285c868b814bfaf9fe4c84f
mercantilism <samuel.galen.gardner@gmail.com>
2018-01-13 17:27:06 -0800

    using filterSelected from props to determine selected filter styles by added classes - .selectedFilter | .deSelectedFilter

--- js/utilComponents/FilterButton.jsx
+++ js/utilComponents/FilterButton.jsx
@@ -2,8 +2,12 @@
 
 import React from 'react';
 
-const FilterButton = (props: { val: string, text: string, updateFilterOption: Function }) => (
-  <button className="bw0 bg-transparent avenir f6 pointer" value={props.val} onClick={props.updateFilterOption}>
+const FilterButton = (props: { filterSelected: boolean, val: string, text: string, updateFilterOption: Function }) => (
+  <button
+    className={`${props.filterSelected ? 'selectedFilter' : 'deSelectedFilter'} bw0 bg-transparent avenir f6 pointer`}
+    value={props.val}
+    onClick={props.updateFilterOption}
+  >
     {props.text}
   </button>
 );
