39a10bf00047828cf5f6b0246b0eda3e9e57e3a0
mercantilism <samuel.galen.gardner@gmail.com>
2018-01-13 17:53:54 -0800

    using join on selectedFilterArray to logically apply Tachions' style classes

--- js/utilComponents/FilterButton.jsx
+++ js/utilComponents/FilterButton.jsx
@@ -2,14 +2,19 @@
 
 import React from 'react';
 
-const FilterButton = (props: { filterSelected: boolean, val: string, text: string, updateFilterOption: Function }) => (
-  <button
-    className={`${props.filterSelected ? 'selectedFilter' : 'deSelectedFilter'} bw0 bg-transparent avenir f6 pointer`}
-    value={props.val}
-    onClick={props.updateFilterOption}
-  >
-    {props.text}
-  </button>
-);
+const FilterButton = (props: { filterSelected: boolean, val: string, text: string, updateFilterOption: Function }) => {
+  const selectedFilterArray = ['b--near-black', 'bb', 'bw2', 'bt-0', 'br-0', 'bl-0'];
+  return (
+    <button
+      className={`${
+        props.filterSelected === true ? selectedFilterArray.join(' ') : 'deSelectedFilter'
+      } mr2 bw0 bg-transparent avenir f6 pointer`}
+      value={props.val}
+      onClick={props.updateFilterOption}
+    >
+      {props.text}
+    </button>
+  );
+};
 
 export default FilterButton;
