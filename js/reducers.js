121833bb138b9e9f1dc2002339c55bf8063a4a5c
mercantilism <samuel.galen.gardner@gmail.com>
2018-01-26 12:46:34 -0800

    updated imports to reflect default export updates from ./actions.jsx; farnersFilter reducer created composed in rootReducer

--- js/reducers.js
+++ js/reducers.js
@@ -2,7 +2,7 @@
 
 import { combineReducers } from 'redux';
 import initialState from './initialState';
-import SET_GREENSPACES_FILTERS from './actions';
+import { SET_GREENSPACES_FILTERS, SET_FARMERS_FILTERS } from './actions';
 
 const user = (state = { id: '5a3074b72b40d9857018bf07' }, action: Action) => {
   if (action) {
@@ -17,7 +17,13 @@ const greenspacesFilters = (state = initialState.filters.greenspaces, action: Ac
   }
   return state;
 };
+const farmersFilters = (state = initialState.filters.farmers, action: Action) => {
+  if (action.type === SET_FARMERS_FILTERS) {
+    return Object.assign({}, state, action.payload);
+  }
+  return state;
+};
 
-const rootReducer = combineReducers({ user, greenspacesFilters });
+const rootReducer = combineReducers({ user, greenspacesFilters, farmersFilters });
 
 export default rootReducer;
