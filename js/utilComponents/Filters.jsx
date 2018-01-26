fdd1d31dfce214dc85d2f13eeabcdab6ad9b809f
mercantilism <samuel.galen.gardner@gmail.com>
2018-01-25 17:17:24 -0800

    Added Filter title and container elements around Filters

--- js/utilComponents/Filters.jsx
+++ js/utilComponents/Filters.jsx
@@ -36,7 +36,7 @@ const Filters = (props: { filterCat: string, filters: {}, updateOptions: Functio
 
   const filterRowProps = FilterCatProps[props.filterCat];
   const filterGroup = (filterRow: any, filterTitle: string) => (
-    <div key="filterTitle" className="mr4 flex justify-start">
+    <div key={filterTitle} className="mr4 flex justify-start">
       <button className="mr3 pl1 pr3 pv1 bg-transparent bt-0 br-0 bb bl-0 bw2 b--green fw6 f5 avenir tl pointer">
         {filterTitle}
       </button>
@@ -59,7 +59,23 @@ const Filters = (props: { filterCat: string, filters: {}, updateOptions: Functio
     filterGroup(filterRow(filter), filterRowProps.titles[index])
   );
 
-  return <div className="flex items-center">{filters}</div>;
+  return (
+    <div className="pt4 pb0">
+      <div className="ph5">
+        <button
+          className="bt-0 br-0 bb-0 bl-0 bg-transparent pointer flex justify-start items-center"
+          style={{ paddingLeft: '0' }}
+        >
+          <h3 className="mr2 f3 fw6 bw0 avenir near-black">Filters</h3>
+          <svg className="w1 near-black" data-icon="chevronDown" viewBox="0 0 32 32" style={{ fill: 'currentcolor' }}>
+            <title>chevronDown icon</title>
+            <path d="M1 18 L5 14 L16 24 L27 14 L31 18 L16 32 Z" />
+          </svg>
+        </button>
+        <div className="flex items-center">{filters}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Filters;
