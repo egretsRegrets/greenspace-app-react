e44f833f587ce62aa4f5e8206af4b210211d27a4
mercantilism <samuel.galen.gardner@gmail.com>
2018-01-27 14:48:34 -0800

    removed container div with padding, padding should be determined by parent

--- js/GreenspaceCardList.jsx
+++ js/GreenspaceCardList.jsx
@@ -9,21 +9,19 @@ const GreenspaceCardList = (props: {
   currentPageNumber: number | null,
   cardsPerPage: number | null
 }) => (
-  <div className="ph5">
-    <section className="flex flex-wrap justify-start pv4">
-      {/* 
+  <section className="flex flex-wrap justify-start pv4">
+    {/* 
             if pagination info was passed in props, then list the greenspace cards sliced for current page and cars per pages
             else just map cards from all of the greenspaces passed - this way we don't have to always specify pagination for greenspaces 
           */}
-      {props.currentPageNumber && props.cardsPerPage
-        ? paginationSlice(props.greenspaceCardList, props.currentPageNumber, props.cardsPerPage).map(
-            (greenspace: Greenspace) => <GreenspaceCard {...greenspace} key={greenspace.id} />
-          )
-        : props.greenspaceCardList.map((greenspace: Greenspace) => (
-            <GreenspaceCard {...greenspace} key={greenspace.id} />
-          ))}
-    </section>
-  </div>
+    {props.currentPageNumber && props.cardsPerPage
+      ? paginationSlice(props.greenspaceCardList, props.currentPageNumber, props.cardsPerPage).map(
+          (greenspace: Greenspace) => <GreenspaceCard {...greenspace} key={greenspace.id} />
+        )
+      : props.greenspaceCardList.map((greenspace: Greenspace) => (
+          <GreenspaceCard {...greenspace} key={greenspace.id} />
+        ))}
+  </section>
 );
 
 export default GreenspaceCardList;
