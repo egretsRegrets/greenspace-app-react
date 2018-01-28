5cd667e44fb754d3f20543db0c21c40321a1d608
mercantilism <samuel.galen.gardner@gmail.com>
2018-01-27 21:51:11 -0800

    filtering props.farmers by user-selected filters

--- js/Farmers.jsx
+++ js/Farmers.jsx
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 import FarmerCardList from './FarmerCardList';
 import Filters from './utilComponents/Filters';
 import { NextPreviousBtns } from './utilComponents/PageControls';
-import { passFilterUpdateToSetter, setFilter } from './utils';
+import { passFilterUpdateToSetter, setFilter, getSelectedFiltersMap, filterStateIsInitial } from './utils';
 import { setFarmersFilters } from './actionCreators';
 
 // $FlowFixMe
@@ -24,6 +24,44 @@ class Farmers extends Component {
     // $FlowFixMe
     this.setState({ pageNumber: parseInt(val, 10) });
 
+  passesExperienceFilter = (farmerExperience: farmingExperienceLevel, selectedFilters: Array<string>) => {
+    if (filterStateIsInitial(selectedFilters)) {
+      return true;
+    }
+    if (selectedFilters.includes(farmerExperience)) {
+      return true;
+    }
+    return false;
+  };
+  passesSkillsFilter = (farmerSkills: Array<farmingSkills> | typeof undefined, selectedFilters: Array<string>) => {
+    if (filterStateIsInitial(selectedFilters)) {
+      return true;
+    }
+    // User/FarmerBrief.skills is an optional property, so we need to account for it not being available
+    if (!farmerSkills) {
+      return false;
+    }
+    // if any of the selectedFilters are in farmerSkills return true - the farmer is filtered in
+    for (let i = 0; i < selectedFilters.length; i += 1) {
+      if (farmerSkills.includes(selectedFilters[i])) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  filterFarmers = () => {
+    const selectedFiltersMap: { experience: Array<string>, skills: Array<string> } = getSelectedFiltersMap(
+      this.props.filters
+    );
+    const filteredFarmers = this.props.farmers.filter(
+      farmer =>
+        this.passesExperienceFilter(farmer.experience, selectedFiltersMap.experience) &&
+        this.passesSkillsFilter(farmer.skills, selectedFiltersMap.skills)
+    );
+    return filteredFarmers;
+  };
+
   render() {
     const cardsPerPage = 8;
 
@@ -37,7 +75,7 @@ class Farmers extends Component {
           }
         />
         <FarmerCardList
-          farmerCardList={this.props.farmers}
+          farmerCardList={this.filterFarmers()}
           // $FlowFixMe
           currentPageNumber={this.state.pageNumber}
           cardsPerPage={cardsPerPage}
@@ -45,7 +83,7 @@ class Farmers extends Component {
         <NextPreviousBtns
           // $FlowFixMe
           pageNumber={this.state.pageNumber}
-          dataLength={this.props.farmers.length}
+          dataLength={this.filterFarmers().length}
           cardsPerPage={cardsPerPage}
           clickHandler={this.updatePage}
         />
